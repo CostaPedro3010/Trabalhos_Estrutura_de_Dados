9acb9ae198bede4156321521815df805fed3f5e7
Pedro Costa <costapedro3010@gmail.com>
2024-03-13 14:08:53 -0300

    Atualização de QuantosPrimos.py

--- Trabalhos/QuantosPrimos.py
+++ Trabalhos/QuantosPrimos.py
@@ -1,43 +1,45 @@
 
-numDigitado = int(input())
-contadorPrimos = 0
-contadorDivisores = 1
+# i = 1
+# totPrimo = 0
+# numPrimo = int(input())
+# if numPrimo == 0 or numPrimo == 1:
+#     print(0)
+# while numPrimo > 1:
+#     numDivisores = 0
+#     while numPrimo >= i:
+#         if numPrimo % i == 0:
+#             numDivisores += 1
+#         i += 1
+#     if numDivisores == 2:
+#         totPrimo += 1
+#     if numPrimo == 0:
+#         break
+#     numPrimo = int(input())
+# print(totPrimo)
 
-while (numDigitado != 0):
-    contadorDivisores = 1
+i = 1
+tot_primo = 0
 
-    if (numDigitado == 0):
-        print(contadorPrimos)
+# Loop principal
+while True:
+    # Obtenha o próximo número a ser verificado
+    num = int(input("Digite um número natural positivo (0 para encerrar): "))
 
-    while contadorDivisores < numDigitado:
-        if numDigitado % contadorDivisores == 0:
-            contadorDivisores += 1
+    # Verifique se o número é primo
+    if num > 1:
+        # Inicialize a contagem de divisores
+        num_divisores = 0
+        # Verifique a divisibilidade de cada número a partir de 2
+        for j in range(2, num):
+            if num % j == 0:
+                num_divisores += 1
+                break
+        # Se o número tiver apenas 2 divisores, ele é primo
+        if num_divisores == 1:
+            tot_primo += 1
+    elif num == 0:
+        # Se 0 for digitado, encerre o loop principal
+        break
 
-    if (contadorDivisores == 2):
-        contadorPrimos += 1
-
-    numDigitado = int(input())
-
-print(contadorPrimos)
-
-#
-#
-# contadorPrimos = 0
-#
-# numDigitado = int(input("Digite um número (ou zero para encerrar): "))
-#
-# while numDigitado != 0:
-#     if numDigitado >= 2:
-#         primo = 1
-#         contadorDivisores = 2
-#
-#         while contadorDivisores <= numDigitado:
-#             if numDigitado % contadorDivisores == 0:
-#                 primo = 0
-#             contadorDivisores += 1
-#
-#         contadorPrimos += primo
-#
-#     numDigitado = int(input("Digite um número (ou zero para encerrar): "))
-#
-# print("Quantidade de números primos digitados:", contadorPrimos)
+# Exiba a contagem de números primos
+print("A quantidade de números primos digitados foi:", tot_primo)
